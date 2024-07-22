515c72acc2a7466a9b18bff2d0885fc633f2e318
Aman Patel <amanpatel.ajp@gmail.com>
2024-07-22 18:11:09 +0530

    day 3 of learning ts

---
+++ 3_Typeof.ts
@@ -0,0 +1,10 @@
+// typeof operator returns the data type of the operand
+
+var no = 101   
+console.log(typeof no);   //number
+
+var str = "Marvellous Infosystems"
+console.log(typeof str);   //string
+
+var value;    
+console.log(typeof value);  //undefined
