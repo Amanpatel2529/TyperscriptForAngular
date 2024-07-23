c4fcc03f86a581f7bb2e9fe8ef69d1b257066f79
Aman Patel <amanpatel.ajp@gmail.com>
2024-07-23 18:05:23 +0530

    day 4 of learning ts

---
+++ 4_VariableDeclarations.ts
@@ -0,0 +1,12 @@
+// Variable declared with data type and initialization
+var name1:string = "Marvellous Infosystems";  
+
+// Variable declared with data type only.
+var name2:string;   //declared without value
+
+// Variable declared without data type. Data type is predicted upon the initialization
+var name3 = "Marvellous Infosystems"; //declared value not type
+
+// Data type is considered as any
+var name4;  //type : any,  value:undefined
+ 
