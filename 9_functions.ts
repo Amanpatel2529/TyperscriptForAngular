2e6ecf88213f8d5dc8fa6771351107526f4528fa
Aman Patel <amanpatel.ajp@gmail.com>
2024-07-28 13:58:24 +0530

    day 9 of learning ts

---
+++ 9_functions.ts
@@ -0,0 +1,15 @@
+// Defination of add function
+
+function add(no1:number,no2:number):number 
+{  
+    var ans:number;
+
+    ans = no1+no2;
+    
+    return ans; 
+}  
+
+var iret:number;
+iret = add(10,1);
+
+console.log("Addition is :"+iret);
