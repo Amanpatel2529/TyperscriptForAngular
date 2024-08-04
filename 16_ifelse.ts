06ad638fc9f0aab8c526f25e99c7f6cec6afd3c2
Aman Patel <amanpatel.ajp@gmail.com>
2024-08-04 17:22:37 +0530

    day 16 of learning ts

---
+++ 16_ifelse.ts
@@ -0,0 +1,18 @@
+ 
+function ChkEvenOdd(num:number) 
+{ 
+    if (num % 2 == 0) 
+    {
+        console.log("Even");
+    } 
+    else 
+    {
+        console.log("Odd");
+    }      
+ }
+
+var no = 12;
+
+ChkEvenOdd(no);
+
+
