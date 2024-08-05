2f547ee9172b2d559a46a1cdda058b3f5d8a22d8
Aman Patel <amanpatel.ajp@gmail.com>
2024-08-05 19:04:08 +0530

    day 17 of learning ts

---
+++ 17_switch.ts
@@ -0,0 +1,28 @@
+var Div:string = "B"; 
+ 
+switch(Div) 
+{ 
+   case "A": 
+   { 
+      console.log("Timing : 7 AM to 11 AM"); 
+      break; 
+   } 
+   
+   case "B": 
+   { 
+      console.log("Timing : 9 AM to 12 AM"); 
+      break; 
+   } 
+   
+   case "C": 
+   {
+      console.log("Timing : 1 PM to 6 PM"); 
+      break;    
+   } 
+     
+   default: 
+   { 
+      console.log("Invalid Division"); 
+      break;              
+   } 
+}
