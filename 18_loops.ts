13121f7c6c0e2a71e3ad7602ac7f365d7d1cd6bf
Aman Patel <amanpatel.ajp@gmail.com>
2024-08-06 14:23:25 +0530

    day 18 of learning ts

---
+++ 18_loops.ts
@@ -0,0 +1,28 @@
+var no:number = 5; 
+var i:number;
+ 
+console.log("for loop demonstration :");
+
+for(i = 1; i<= no; i++) 
+{
+   console.log(i);
+}
+
+console.log("while loop demonstration :");
+i = 1;
+
+while(i<=no)
+{
+    console.log(i);
+    i++;
+}
+
+console.log("do-while loop demonstration :");
+i = 1;
+
+do
+{
+    console.log(i);
+    i++;
+}while(i<=no);
+
